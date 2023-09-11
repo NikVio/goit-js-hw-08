a0ad87e5f2e5e0b61b254a3f93fc370281b13ed7
NikVio <131026216+NikVio@users.noreply.github.com>
2023-09-11 21:54:36 +0300

    start hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,56 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+
+
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
 console.log(galleryItems);
+
+
+
+const galleryElements = document.querySelector('.gallery');
+
+
+const createMarkup = createPhotoGallery(galleryItems);
+
+galleryElements.insertAdjacentHTML('beforeend', createMarkup);
+
+galleryElements.addEventListener('click', onLightboxClick);
+
+
+
+function createPhotoGallery(galleryItems) {
+    return galleryItems
+        .map(({ original, preview, description }) => {
+            return `
+        
+        <li class="gallery__item">
+             <a class="gallery__link" href="${original}">
+                 <img
+                     class="gallery__image"
+                    src="${preview}"
+                     alt="${description}"
+                 />
+             </a>
+       </li>
+    `;
+        }).join('');
+}  
+
+
+
+function onLightboxClick(evt) {
+    evt.preventDefault();  
+}
+
+lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250
+    })
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,9 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+console.log(throttle);
+
+const video = document.querySelector('iframe');
+
+console.log(video);
 
