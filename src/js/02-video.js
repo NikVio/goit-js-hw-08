2624e9017b71318426c20003a3dcf92c962896d5
NikVio <131026216+NikVio@users.noreply.github.com>
2023-09-17 15:49:40 +0300

    end hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,21 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-console.log(throttle);
+
 
 const video = document.querySelector('iframe');
 
+const TIME_KEY = "videoplayer-current-time";
 console.log(video);
 
+const player = new Player(video);
+
+player.on('timeupdate', throttle(onPlayVideo, 1000));
+
+const saveTime = JSON.parse(localStorage.getItem(TIME_KEY)) || 0;
+player.setCurrentTime(saveTime);
+
+function onPlayVideo(data) {
+    const videoLocStore = data.seconds;
+    localStorage.setItem(TIME_KEY, JSON.stringify(videoLocStore));
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,74 +3,38 @@ import throttle  from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 
 
-form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('input', throttle(onInputForm, 500));
+form.addEventListener('submit', onSubmitForm);
 
-form.addEventListener('submit', onBtnSubmit)
+const FORM_KEY = "feedback-form-state";
 
-
-
-let FORM_KEY = "feedback-form-state";
-const parse = JSON.parse(localStorage.getItem(FORM_KEY)) || [];
-
+const formData = JSON.parse(localStorage.getItem(FORM_KEY)) || {};
 
 checkForm();
 
-
-
-function onFormInput(evt) {
-   
-    // const { email, message } = form.elements;
+function onInputForm(evt) {
+    formData[evt.target.name] = evt.target.value;
+  
     
-    //     const data = {
-    //      mail: email.value,
-    //      message: message.value,
-    //     };
-    
-    parse[evt.target.name] = evt.target.value;
-       localStorage.setItem(FORM_KEY, JSON.stringify(parse));   
-}
-
-
-
-function onBtnSubmit(evt) {
-    evt.preventDefault();
-    
-    
-    
-  if (!(form.email.value) || !(form.message.value)) {
-      alert('All fields must be filled out');
-      return
-  }
-     
-//   const { email, message } = form.elements;
-    
-//         const data = {
-//          mail: email.value,
-//          message: message.value,
-//         };
-    
-    
-//   console.log(data);
-    
-    localStorage.removeItem(FORM_KEY);
-    evt.target.reset();
-
-    
+    localStorage.setItem(FORM_KEY, JSON.stringify(formData));
 }
 
 function checkForm() {
     const { email, message } = form.elements;
-    if (parse) {
-        email.value = parse.email;
-        message.value = parse.message;
-    }
-
+     if (formData) {
+         email.value = formData.email || "";
+         message.value = formData.message || "";
+     }
 }
 
-
-
-
-
-
-
+function onSubmitForm(evt) {
+    evt.preventDefault();
+    if (!(form.email.value) || !(form.message.value)) {
+         alert('All fields must be filled out');
+         return;
+     };
+     console.log(formData);
+     evt.target.reset();
+     localStorage.removeItem(FORM_KEY);
+}
 
