7e32222b149064a1240eb960682b17835357ab4d
NikVio <131026216+NikVio@users.noreply.github.com>
2023-09-19 13:06:52 +0300

    second end

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,8 @@
 import throttle  from 'lodash.throttle';
 
+let formData = {};
+const LS = localStorage;
+
 const form = document.querySelector('.feedback-form');
 
 
@@ -8,44 +11,24 @@ form.addEventListener('submit', onSubmitForm);
 
 const FORM_KEY = "feedback-form-state";
 
-const formData = JSON.parse(localStorage.getItem(FORM_KEY)) || {};
 
-// checkForm();
 
 function onInputForm(evt) {
     formData[evt.target.name] = evt.target.value;
-    // const { email, message } = form.elements;
-    // const data = {
-    //     mail: email.value,
-    //     message: message.value,
-    // }
-  
+    LS.setItem(FORM_KEY, JSON.stringify(formData));
+ 
+}
+ 
+if (LS.getItem(FORM_KEY)) {
+    formData = JSON.parse(LS.getItem(FORM_KEY))
     
-    localStorage.setItem(FORM_KEY, JSON.stringify(formData));
+    for (let key in formData) {
+        form.elements[key].value = formData[key];
+    }
+
+  
 }
 
- if (formData) {
-        
-         
-         for (let key in formData) {
-             form.elements[key].value = formData[key];
-       
-         }
-     }
-
-// function checkForm() {
-//     // const { email, message } = form.elements;
-//      if (formData) {
-//         //  email.value = formData.email || "";
-        
-//         //  message.value = formData.message || "";
-         
-//          for (let key in formData) {
-//              form.elements[key].value = formData[key];
-       
-//          }
-//      }
-// }
 
 function onSubmitForm(evt) {
     evt.preventDefault();
@@ -56,6 +39,6 @@ function onSubmitForm(evt) {
      console.log(formData);
      evt.target.reset();
     localStorage.removeItem(FORM_KEY);
-    
+    formData = {};
 }
 
